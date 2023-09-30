0395f11f7c8d95ae7287fde2bba29166fc071662
7AleGz <7alegz@gmail.com>
2023-09-29 22:43:45 -0300

    VB code added

---
+++ 11- Parcial 13.10/FrmLogin.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.psw = New System.Windows.Forms.TextBox()
+        Me.user = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(120, 71)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Usuario"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(123, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Contraseña"
+        '
+        'psw
+        '
+        Me.psw.Location = New System.Drawing.Point(258, 119)
+        Me.psw.Name = "psw"
+        Me.psw.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.psw.Size = New System.Drawing.Size(100, 20)
+        Me.psw.TabIndex = 2
+        '
+        'user
+        '
+        Me.user.Location = New System.Drawing.Point(258, 63)
+        Me.user.Name = "user"
+        Me.user.Size = New System.Drawing.Size(100, 20)
+        Me.user.TabIndex = 3
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(515, 92)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Ingresar"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'FrmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(644, 263)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.user)
+        Me.Controls.Add(Me.psw)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FrmLogin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FrmLogin"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents psw As TextBox
+    Friend WithEvents user As TextBox
+    Friend WithEvents btnLogin As Button
+End Class
---
+++ 11- Parcial 13.10/FrmLogin.vb
@@ -0,0 +1,14 @@
+﻿Public Class FrmLogin
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        If user.Text = "izo" And psw.Text = "123" Then
+            FrmMain.Show()
+            Me.Close()
+
+        Else
+            MsgBox("Usuario o contraseña incorrecta")
+            user.Text = ""
+            psw.Text = ""
+            user.Focus()
+        End If
+    End Sub
+End Class
