40fb9d700cf9f5c0e5e6719d8a33c286157a814f
7AleGz <7alegz@gmail.com>
2023-10-11 22:43:18 -0300

    ModalUSDValue fixed

--- 11- Parcial 13.10/components/ModalUSDValue.vb
+++ 11- Parcial 13.10/components/ModalUSDValue.vb
@@ -6,18 +6,13 @@
 
     Public ReadOnly Property USDValue() As Double
         Get
-            If Not IsNumeric(Price.Text) Then
-                MsgBox("ingresa un valor válido")
+            Dim dolarValue As Double
+            If Double.TryParse(Price.Text, dolarValue) Then
+                Return dolarValue.ToString()
             Else
-                Dim dolarValue = Price.Text
+                MessageBox.Show("Por favor, ingresa una cantidad válida para comprar.")
+                Return 0
             End If
-            'Dim dolarValue As Double
-            'If Double.TryParse(Price.Text, dolarValue) Then
-            'Return dolarValue.ToString()
-            'Else
-            'MessageBox.Show("Por favor, ingresa una cantidad válida para comprar.")
-            '   Return 0
-            'End If
         End Get
     End Property
 End Class
